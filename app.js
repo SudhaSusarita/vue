5e01761b2698151624f0f5c428b22df96bf5178e
Sudha <s.balaguru@bangor.ac.uk>
2021-06-03 14:45:59 +0100

    Added markers with backend connection

--- app.js
+++ app.js
@@ -1,31 +1,49 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoic3VkaGFzdXNhcml0YSIsImEiOiJja3BiZHZqazcweDh0MnJsbGh3ZXN5bjlhIn0.ORkW9gCJg_4youJQ5pxGIQ';
-    const map = new mapboxgl.Map({
-        container: "map",
-        style: "mapbox://styles/mapbox/dark-v9",
-        center: [52.76,-4.75],
-      maxBounds: [
-        [-14.835942,49.156329],
-        [5.378902,59.974586],
-        
-      ],
-      });
-      map.on('load', () => {
-      // TODO: Here we want to load a layer
-      // TODO: Here we want to load/setup the popup
-      });
-      const nav = new mapboxgl.NavigationControl();
-        map.addControl(nav, "top-right");
-      const geolocate = new mapboxgl.GeolocateControl({
-        positionOptions: {
-          enableHighAccuracy: true
-        },
-        trackUserLocation: true
-      });
 
-      map.addControl(geolocate, "top-right");
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/satellite-streets-v11',
+      center: [-4.5, 56.4], // starting position [lng, lat]
+      zoom: 5.2 // starting zoom
+  });
+  map.on('load', () => {
+    var geojson;
+    let request = new XMLHttpRequest();
+    request.open("GET","http://192.168.32.1:5005/api/sightings");
+    request.send();
+    request.onload =() => {
+      console.log(request);
+      if(request.status ===200){
+        geojson =JSON.parse(request.response);
+        geojson.forEach(function (marker) {
+
+let coordinates = [marker.longitude,marker.latitude];
+console.log(coordinates);       // create a HTML element for each feature
+          var el = document.createElement('div');
+          el.className = 'marker';
+           
+          // make a marker for each feature and add it to the map
+          new mapboxgl.Marker(el)
+          .setLngLat(coordinates)
+          .setPopup(
+          new mapboxgl.Popup({ offset: 25 }) // add popups
+          .setHTML(
+'<table class = marker-info><tbody><tr><th>DATE</th><td>'
++marker.sightingDate +'</td></tr>'+
+'<tr><th>SPECIES</th><td>'+marker.speciesCode +'</td></tr>'+
+'<tr><th>ESTIMATE</th><td>'+marker.estimate +'</td></tr>'+
+'<tr><th>OBSERVER</th><td>'+marker.name +'</td></tr>'+
+'</tbody></table>'
+
+        
+          )
+          )
+          .addTo(map);
+          });
+      }else{
+        console.log("error")
+      }
+    }
+  
    
-       var el= document.createElement('div'); 
-       el.style.width ='30px';
-       el.style.height='30px';
-       el.style.borderRadius = '50%';   
-       new mapboxgl.Marker(el).setLngLat([52.5325,-4.5918]).addTo(map);
+    });
